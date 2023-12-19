d4663e9123124178a50a2c65117561cf53c68b0c
Jake Rote <jake.rote1995@gmail.com>
2023-12-19 16:54:48 +0000

    Test fix

--- components/hlk_ld2450/hlk_ld2450.h
+++ components/hlk_ld2450/hlk_ld2450.h
@@ -23,7 +23,6 @@ namespace esphome
       void update() override {
         if (this->illuminance_sensor_ != nullptr)
           this->illuminance_sensor_->publish_state(42.0);
-        this->publish_state(50.0);
       }
     protected:
       /// Read the illuminance value and store the calculated ambient illuminance in t_fine.
--- components/hlk_ld2450/sensor.py
+++ components/hlk_ld2450/sensor.py
@@ -18,11 +18,13 @@ HLKLD2450 = hlk_ld2450_ns.class_(
     "HLKLD2450", sensor.Sensor, cg.PollingComponent, uart.UARTDevice
 )
 
+CONF_ILLUMINANCE = "illuminance"
+
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.Required(CONF_ID): cv.declare_id(HLKLD2450),
-            cv.Optional("illuminance"): sensor.sensor_schema(
+            cv.Optional(CONF_ILLUMINANCE): sensor.sensor_schema(
                 HLKLD2450,
                 unit_of_measurement=UNIT_LUX,
                 accuracy_decimals=1,
@@ -41,3 +43,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
+    
+    if voltage_config := config.get(CONF_ILLUMINANCE):
+        sens = await sensor.new_sensor(voltage_config)
+        cg.add(var.set_illuminance_sensor(sens))
