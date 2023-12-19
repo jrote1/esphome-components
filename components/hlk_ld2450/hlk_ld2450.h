94f120a920b69e769bedb03a7b931f70921964df
Jake Rote <jake.rote1995@gmail.com>
2023-12-19 15:49:02 +0000

    Test fix

--- components/hlk_ld2450/hlk_ld2450.h
+++ components/hlk_ld2450/hlk_ld2450.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "esphome.h"
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome
 {
