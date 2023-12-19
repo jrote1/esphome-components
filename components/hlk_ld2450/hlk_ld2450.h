d67a9a6f49100bac4fae5943ca0f1b6218d590ec
Jake Rote <jake.rote1995@gmail.com>
2023-12-19 16:03:54 +0000

    Test fix

--- components/hlk_ld2450/hlk_ld2450.h
+++ components/hlk_ld2450/hlk_ld2450.h
@@ -14,14 +14,20 @@ namespace esphome
     public:
       // constructor
       HLKLD2450() : PollingComponent(15000) {}
+      void set_illuminance_sensor(sensor::Sensor *illuminance_sensor) { illuminance_sensor_ = illuminance_sensor; }
 
       void setup() override
       {
         // This will be called by App.setup()
       }
       void update() override {
-        publish_state(42.0);
+        this->illuminance_sensor_->publish_state(42.0);
       }
+    protected:
+      /// Read the illuminance value and store the calculated ambient illuminance in t_fine.
+      float read_illuminance_(const uint8_t *data, int32_t *t_fine);
+
+      sensor::Sensor *illuminance_sensor_{nullptr};
     };
 
   } // namespace hlk_ld2450
--- components/hlk_ld2450/sensor.py
+++ components/hlk_ld2450/sensor.py
@@ -1,7 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_ID,
+    CONF_TEMPERATURE
+)
 
 DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@jrote1"]
@@ -16,19 +19,10 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.Required(CONF_ID): cv.declare_id(HLKLD2450),
+            cv.Optional("illuminance"): sensor.sensor_schema()
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
-    .extend(
-        {
-            cv.Optional("resolution"): cv.invalid(
-                "The 'resolution' option has been removed. The optimal value is now dynamically calculated."
-            ),
-            cv.Optional("measurement_duration"): cv.invalid(
-                "The 'measurement_duration' option has been removed. The optimal value is now dynamically calculated."
-            ),
-        }
-    )
     .extend(cv.polling_component_schema("60s"))
     .extend(uart.UART_DEVICE_SCHEMA),
 )
